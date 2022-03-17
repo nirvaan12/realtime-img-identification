f687c3cad6093833f2328fb4df2edd6c407d4332
nirvaan12 <85714636+nirvaan12@users.noreply.github.com>
2022-03-17 19:58:23 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -3,7 +3,38 @@ function setup() {
   canvas.center();
   video = createCapture(VIDEO);
   video.hide();
+  classifier=ml5.imageClassifier("MobileNet",modelLoaded);
 }
 
 
 
+function draw(){
+  image(video,0,0,300,300);
+  classifier.classify(video,gotResult);
+}
+
+function modelLoaded(){
+  console.log("Model is loaded");
+}
+previousresult="";
+
+function gotResult(error,results){
+  if(error){
+    console.error(error);
+  }
+  else{
+  if((results[0].confidence>0.5)&&(previousresult != results[0].label)){
+   
+  console.log(results);
+  previousresult=results[0].label;
+  var synth=window.speechSynthesis;
+  speakdata="object detected is "+results[0].label;
+  var utterThis=new SpeechSynthesisUtterance;
+  synth.speak(utterThis);
+  
+  document.getElementById("result_object_name").innerHTML=results[0].label;
+  document.getElementById("result_object_accuracy").innerHTML=results[0].confidence.toFixed(3);
+
+  }
+}
+}
